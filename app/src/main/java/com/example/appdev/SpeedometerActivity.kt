5e2b61dfc783f917bdb81290fca94f609df4a4bd
Nico <58952423+Wrdlprmpf@users.noreply.github.com>
2022-06-16 18:00:24 +0200

    Implemented Speedometer
    Finished with speedometer in km/h avg and topspeed too.

--- app/src/main/java/com/example/appdev/SpeedometerActivity.kt
+++ app/src/main/java/com/example/appdev/SpeedometerActivity.kt
@@ -9,7 +9,6 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
@@ -18,69 +17,90 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 
-class SpeedometerActivity : AppCompatActivity(),LocationListener {
-    lateinit var lm:LocationManager
-    lateinit var speedOutput: TextView
+class SpeedometerActivity : AppCompatActivity(), LocationListener {
+	lateinit var lm: LocationManager
+	lateinit var speedOutput: TextView
+	var topSpeed: Float = 0F
+	var averageSpeed: Float = 0F
+	lateinit var topSpeedOutput: TextView
+	lateinit var averageSpeedOutput: TextView
 
-    @SuppressLint("MissingPermission")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_speedometer)
-        speedOutput = findViewById(R.id.speedometerOutput)
-        println("Gets Here1---------------------------------")
-        permissionCheck()
+	var speeds = ArrayList<Float>()
 
-        lm = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        println("Gets Here2---------------------------------")
+	@SuppressLint("MissingPermission")
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_speedometer)
+		speedOutput = findViewById(R.id.speedometerOutput)
+		topSpeedOutput = findViewById(R.id.topSpeedOutput)
+		averageSpeedOutput = findViewById(R.id.averageSpeedOutput)
+		permissionCheck()
+		lm = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0F,this)
-        var isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        Log.d("Tag","Gets Here LOG --------------------------")
-        val lm = this.getSystemService(LOCATION_SERVICE) as LocationManager
-    }
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0F, this)
+		var isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		val lm = this.getSystemService(LOCATION_SERVICE) as LocationManager
+	}
 
-    fun permissionCheck(){
-        try {
-            if (ContextCompat.checkSelfPermission(
-                    applicationContext,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    101
-                )
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+	fun permissionCheck() {
+		try {
+			if (ContextCompat.checkSelfPermission(
+					applicationContext,
+					Manifest.permission.ACCESS_FINE_LOCATION
+				) != PackageManager.PERMISSION_GRANTED
+			) {
+				ActivityCompat.requestPermissions(
+					this,
+					arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+					101
+				)
+			}
+		} catch (e: Exception) {
+			e.printStackTrace()
+		}
+	}
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.main, menu)
+		return super.onCreateOptionsMenu(menu)
+	}
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menuOptions -> startActivity(Intent(this,OptionsActivity::class.java))
-            R.id.menuMain -> startActivity(Intent(this,MainActivity::class.java))
-        }
-        return super.onOptionsItemSelected(item)
-    }
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		when (item.itemId) {
+			R.id.menuOptions -> startActivity(Intent(this, OptionsActivity::class.java))
+			R.id.menuMain -> startActivity(Intent(this, MainActivity::class.java))
+		}
+		return super.onOptionsItemSelected(item)
+	}
 
-    override fun onLocationChanged(location: Location) {
-        if(location==null){
-            speedOutput.text = "0"
-            println("IF")
-        }
-        else{
-            speedOutput.text = ((location.getSpeed()*3600)/1000).toString()
-            println("ELSE")
-        }
+	override fun onLocationChanged(location: Location) {
+		if (location == null) {
+			speedOutput.text = "0 km/h"
+			println("0")
+			speeds.add(0F)
+		} else {
+			var speed = (location.getSpeed() * 3600) / 1000
+			speedOutput.text = speed.toString()
+			maximumSpeed(speed)
+			speeds.add(speed)
+			println(speed.toString())
+			println(location.latitude.toString())
+		}
+		averageSpeed()
+	}
 
-    }
+	fun maximumSpeed(speed: Float) {
+		if (speed > topSpeed) {
+			topSpeed = speed
+			topSpeedOutput.text = topSpeed.toString()
+		}
+	}
 
-
+	fun averageSpeed() {
+		var avg = 0F;
+		for (s in speeds) {
+			avg += s
+		}
+		averageSpeedOutput.text = (avg / speeds.size).toString()
+	}
 }
